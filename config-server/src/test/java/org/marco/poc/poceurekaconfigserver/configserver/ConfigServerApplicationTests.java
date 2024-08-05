d9e8a742ba13ec3767dbe755ba964540f2f73e15
Marco Antonio Béjar Villalba <marco.antonio.bejar.villalba@gmail.com>
2024-08-05 14:42:49 -0600

    first commit

---
+++ config-server/src/test/java/org/marco/poc/poceurekaconfigserver/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marco.poc.poceurekaconfigserver.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
